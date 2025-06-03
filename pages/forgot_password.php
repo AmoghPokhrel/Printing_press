3d12d6c44ee262a7ae6e61b8b92d6d2a96b8aa42
amoghPokhrel <amoghpokhrel002@gmail.com>
2025-06-03 15:20:45 +0545

    Update forgot_password.php

--- pages/forgot_password.php
+++ pages/forgot_password.php
@@ -27,8 +27,8 @@ if (isset($_POST['request_otp'])) {
             $mail->isSMTP();
             $mail->Host = "smtp.gmail.com"; 
             $mail->SMTPAuth = true;
-            $mail->Username = 'amogh0012@gmail.com'; 
-            $mail->Password = 'ntyr alnh sono dhin'; 
+            $mail->Username = 'Your Email'; 
+            $mail->Password = 'Your SMTP password'; 
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             $mail->Port = 587;
 
